f4e1b3e306d6c798b73af57f5aa995adf150a43d
Stephen Snyder <ssnyder555@live.com>
2018-10-15 13:18:43 -0500

    messing with it

--- labs-wk-5/update-delete/server.js
+++ labs-wk-5/update-delete/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const Objects = require('./models/objects')
-const methodOverride = require('method-override')
+const Objects = require('./models/objects');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 
 app.use(methodOverride('_method'));
@@ -20,7 +21,19 @@ app.delete('/objects/:index', (req, res) => {
   Objects.splice(req.params.index, 1)
   res.redirect('/objects');
 })
+ // app.post('/', (req, res) => {
+ //   console.log(req.body, "this is where info lives");
+ //
+ //   Objects.push(req.body);
+ //  res.redirect('/objects'
+ // })
 
+ app.get('/:id/edit', (req, res) => {
+   res.render('edit.ejs', {
+     classmates: Objects[req.params.id],
+     id: req.params.id
+   });
+ });
 
 app.listen(3000, () => {
   console.log("I am listening")
---
+++ labs-wk-5/update-delete/views/edit.ejs
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+  <h1>edit classroom</h1>
+
+  <form method="POST" action="/objects/<%=id%>?_method=PUT">
+
+    Name: <input type="text" name="name" value="<%=objects.name%>"/><br/>
+
+            Age: <input type="text" name="age" value="<%=objects.age%>"/><br/>
+
+            Numbers:
+    <input type="checkbox" name="numbers"
+              value="<%=objects.numbers%>"/><br/>
+
+    <% } %>
+
+
+    <input type="submit" name="" value="Submit Changes"/>
+        </form>
+</body>
+
+</html>
